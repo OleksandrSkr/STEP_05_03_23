976ec4138b9b8a747d6215b2330c8f566c1e4c07
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-03-04 14:50:40 +0200

    HW_2

---
+++ homework_2.py
@@ -0,0 +1,7 @@
+number_month = input("Enter the number of the month of the year : ")
+if int(number_month) == 1 :
+    print("now is january")
+elif int(number_month) == 2 :
+    print("now is february")
+else :
+    print("you don't know the months of the year")
