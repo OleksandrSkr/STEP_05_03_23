1874ec0ebedb28e3fd06dc59f4075ca8535acc85
OleksandrSkr <skrigunaleksandr@gmail.com>
2023-03-04 14:40:50 +0200

    HW_1_done

---
+++ homework.py
@@ -0,0 +1,18 @@
+nomber_day = input("Enter the day of the week number : ")
+if int(nomber_day) == 1 :
+    print("today is monday")
+elif int(nomber_day) == 2 :
+    print("today is tuesday")
+elif int(nomber_day) == 3 :
+    print("today is wednesday")
+elif int(nomber_day) == 4 :
+    print("today is thursday")   
+elif int(nomber_day) == 5 :
+    print("today is friday")
+elif int(nomber_day) == 6 :
+    print("today is satarday")
+elif int(nomber_day) == 7 :
+    print("today is sunday")
+else:
+    print("you can't count to seven")
+
